40afa1718cc3c4837fd1f9341b449b5b8131815a
awabaqib96 <55086080+awabaqib96@users.noreply.github.com>
2020-05-04 18:03:11 +0500

    Delete dexCompleteInsert.sql

--- dex/parts/2/dexCompleteInsert.sql
+++
@@ -1,6 +0,0 @@
-INSERT `ethereum-262008.ethupdated.dex2` (from_address, nonce, to_address, gas, gas_price, value, block_number, block_timestamp)
-(SELECT from_address, nonce, to_address, gas, gas_price, value, block_number, block_timestamp
-FROM `bigquery-public-data.crypto_ethereum.transactions`
-WHERE from_address= "0xa7a7899d944fe658c4b0a1803bab2f490bd3849e"
-ORDER BY block_timestamp DESC
-LIMIT 1000000)
