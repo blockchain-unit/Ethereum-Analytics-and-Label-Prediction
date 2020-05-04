ca4e91f407455c1971435eafc51cd24c47795735
awabaqib96 <55086080+awabaqib96@users.noreply.github.com>
2020-05-04 18:03:06 +0500

    Delete insert.sql

--- dex/insert.sql
+++
@@ -1,7 +0,0 @@
-INSERT `ethereum-262008.ethupdated.dex` (from_address, nonce, to_address, gas, gas_price, value, block_number, block_timestamp)
-(SELECT from_address, nonce, to_address, gas, gas_price, value, block_number, block_timestamp
-FROM `bigquery-public-data.crypto_ethereum.transactions`
-WHERE from_address= xyz
-ORDER BY block_timestamp DESC
-)
-UNION ALL
